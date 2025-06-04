e2820a68b23d82094359506aee3da10f9a4882eb
angelrivera12689 <angelfaridr1@gmail.com>
2025-06-04 16:49:19 -0500

    first commit

---
+++ src/test/java/com/example/AuthForge/AuthForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
